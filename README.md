bbdfb53c66d097809c45a0c85112078c99f0b2bc
Matt Caulfield <mcaulfield@gmail.com>
2017-07-10 23:45:32 -0400

    Set assignee owner field when Defect created

--- README.md
+++ README.md
@@ -2,8 +2,22 @@
 
 One-way sync from github issues to defects in CA Agile Central (formerly Rally).
 
+```
+Usage of github2rally:
+  -github-owner string
+        Github user or organization
+  -github-repo string
+        Github repository name
+  -rally-api-key string
+        CA Agile Central (Rally) API Key
+```
+
+## Syncing Submitter and Assignee Fields
+
+Correlating Github users and Rally users requires that each Rally user set their Middle Name field in their profile to match their github username.
+
 ## To-do
-- [ ] Set assignee field when Defect is created
+- [X] Set assignee field when Defect is created
 - [ ] Update Defect when title or assignee changes
 - [ ] Link to github issue in Defect description
 - [ ] Allow user to set Rally workspace
--- sync/sync.go
+++ sync/sync.go
@@ -40,22 +40,40 @@ func SyncDefects(rallyApiKey string, owner string, repo string) {
 		log.Println("Adding new Defect to Rally:", *issue.Title)
 
 		// Query rally for a user that matches the github issue user
-		q = fmt.Sprintf("(MiddleName = %v)", *issue.User.Login)
-		qr, err = rc.QueryUser(q)
-		if err != nil {
-			log.Println(err)
-			continue
+		submitter := ""
+		if issue.User != nil {
+			q = fmt.Sprintf("(MiddleName = %v)", *issue.User.Login)
+			qr, err = rc.QueryUser(q)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			if qr.Count != 0 {
+				submitter = qr.Results[0].Ref
+			}
 		}
-		var userRef string
-		if qr.Count != 0 {
-			userRef = qr.Results[0].Ref
+
+		// Query rally for a user that matches the github assignee
+		assignee := ""
+		if issue.Assignee != nil {
+			q = fmt.Sprintf("(MiddleName = %v)", *issue.Assignee.Login)
+			qr, err = rc.QueryUser(q)
+			if err != nil {
+				log.Println(err)
+			}
+			if qr.Count != 0 {
+				assignee = qr.Results[0].Ref
+			}
 		}
 
 		// Construct a new defect struct
 		d := rally.Defect{}
 		d.Name = fmt.Sprintf("%s #%v", *issue.Title, *issue.Number)
-		if userRef != "" {
-			d.SubmittedBy = &rally.Ref{Ref: userRef, Type: "User"}
+		if submitter != "" {
+			d.SubmittedBy = &rally.Ref{Ref: submitter, Type: "User"}
+		}
+		if assignee != "" {
+			d.Owner = &rally.Ref{Ref: assignee, Type: "User"}
 		}
 		d.ScheduleState = "Defined"
 		d.State = "Submitted"
